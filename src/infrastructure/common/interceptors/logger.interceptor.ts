a6eff84642585a8ff4210b19117513509a4a2297
acordovam <alejandrocordova198@gmail.com>
2022-12-16 20:11:18 -0600

    Added interceptors

---
+++ src/infrastructure/common/interceptors/logger.interceptor.ts
@@ -0,0 +1,40 @@
+import { CallHandler, ExecutionContext, Injectable } from '@nestjs/common';
+import { LoggerService } from '../../logger/logger.service';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor {
+  constructor(private readonly logger: LoggerService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest();
+    const ip = this.getIP(request);
+
+    this.logger.log(
+      `Incoming Request on ${request.path}`,
+      `method=${request.method} ip=${ip}`,
+    );
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(
+          `End Request for ${request.path}`,
+          `method=${request.method} ip=${ip} duration=${Date.now() - now}ms`,
+        );
+      }),
+    );
+  }
+
+  private getIP(request: any): string {
+    let ip: string;
+    const ipAddr = request.headers['x-forwarded-for'];
+    if (ipAddr) {
+      const list = ipAddr.split(',');
+      ip = list[list.length - 1];
+    } else {
+      ip = request.connection.remoteAddress;
+    }
+    return ip.replace('::ffff:', '');
+  }
+}
